decbdb35764c5b7e69a83c14fe2296a0dada4739
Azul-20 <rael.rivero01@gmail.com>
2024-11-07 09:51:45 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ProductComponent } from './components/product/product.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'brands', component: BrandsComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ProductComponent } from './components/product/product.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ServicesComponent,
+    ProductComponent,
+    BrandsComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
